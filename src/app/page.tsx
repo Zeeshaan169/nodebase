8fd07ac66a5cb51896361399076d365c29e2cdec
Zeeshaan1690 <zeeshaanbagwan169@gmail.com>
2025-11-02 07:51:13 +0530

    01: setting up

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button }  from "@/components/ui/button";
+
+
+const page = () =>{
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button>
+        click me 
+      </Button>
+    </div>
+  );
+};
+
+export default page ;
